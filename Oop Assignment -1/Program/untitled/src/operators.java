d8538a71828f134de29999606d9cc3f8d7b5d4f3
Saurab Chaulagai <chaulagainsaurab03@gmail.com>
2026-06-28 17:28:58 +0545

    p

---
+++ Oop Assignment -1/Program/untitled/src/operators.java
@@ -0,0 +1,21 @@
+public class operators {
+    public static void main(String[] args){
+        int a= 10;
+        int b =7;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+
+
+
+    }
+}
